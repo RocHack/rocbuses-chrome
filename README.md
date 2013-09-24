56aba2960c77b82c989afa7c47f513c91cf09dff
David Bang <seonggbang@gmail.com>
2013-09-24 02:20:28 -0400

    reallocation of files / figuring out # of routes.
    figuring out # of routes means that it can find the individual routes.
    So, next in scheduleRender, it'll be about actually inserting the
    schedule table

--- README.md
+++ README.md
@@ -6,3 +6,13 @@ Chrome extension of RocBuses
 Original : github.com/RocHack/rocbuses
 
 URL : rochack.org/rocbuses
+
+===============
+
+To-do:
+
+1. Render the schedule in table format
+
+2. Finish the formatter object
+
+3. Add "highlighting location"
--- core/popup.js
+++ core/popup.js
@@ -1,14 +1,45 @@
+/*
+  Import/refactoring of RocBuses by Rochack
+*/
+
+var formatInfo = {
+
+  dayName : {
+    M: "Monday",
+    T: "Tuesday",
+    W: "Wednesday",
+    R: "Thursday",
+    F: "Friday",
+    S: "Saturday",
+    U: "Sunday"
+  },
+
+  formatLineName : function(line, days) {
+    return line[0].toUpperCase() + line.substr(1) + " Line";
+  }
+
+
+};
+
 var scheduleRender = {
     
-    renderLine : function(data, lineDOM) {
-      console.log(data);
+    renderRoutes : function(name, line, lineDOM) {
+      for (var route in line.routes) {
+        var lineName = formatInfo.formatLineName(name, route.days);
+        var h3 = document.createElement("h3");
+        h3.setAttribute('class', "line_name");
+        h3.appendChild(document.createTextNode(lineName));
+        lineDOM.appendChild(h3);
+        var routeTable = document.createElement('table');
+      }
     },
 
-    renderSchedule : function(schedule, scheduleDOM) {
+    renderLines : function(schedule, scheduleDOM) {
       for (var line in schedule) {
         var lineDOM = document.createElement('div');
         lineDOM.setAttribute('id', line);
-        this.renderLine(schedule[line].routes, lineDOM);
+        lineDOM.setAttribute('class', 'schedule');
+        this.renderRoutes(line, schedule[line], lineDOM);
         scheduleDOM.appendChild(lineDOM);
       }
     },
@@ -16,19 +47,16 @@ var scheduleRender = {
 
     getSchedule : function() {
       var curObj = this;
-      $.getJSON("schedules.json", function(json) {
+      $.getJSON("core/schedules.json", function(json) {
         var schedules = document.createElement('div');
-        schedules.setAttribute('id', 'schedule');
-        curObj.renderSchedule(json, schedules);
+        curObj.renderLines(json, schedules);
         document.body.appendChild(schedules);
       });
     },
-
 };
+
+
 
 document.addEventListener('DOMContentLoaded', function () {
   scheduleRender.getSchedule();
 });
-
-
-
